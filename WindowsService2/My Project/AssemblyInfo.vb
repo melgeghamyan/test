fc0337027610be3121b38a5b85c9d2d7638d66bc
melgeghamyan <mgeghamyan@nairisoft.am>
2013-09-29 19:40:13 +0400

    test commit

---
+++ WindowsService2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsService2")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Hewlett-Packard")> 
+<Assembly: AssemblyProduct("WindowsService2")> 
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("168b3d7b-4579-4b55-ab40-301cb6fa8f33")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
